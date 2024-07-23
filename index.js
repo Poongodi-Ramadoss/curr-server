ef348cc34e3bc2affafe1e3d82f114f8d5f5464c
Poongodi <poongodi@Poongodis-MacBook-Pro.local>
2024-07-23 12:05:03 +0100

    Add logger

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Initialisation of the server
-
 require("dotenv").config();
 const app = require("./app");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
